6c1b8544e28504e8d6957a1a8957de9e44f9ba40
Harshitkumar22 <harshitkumar1463@gmail.com>
2025-01-29 16:56:50 +0530

    Add files via upload

---
+++ Pattern1.java
@@ -0,0 +1,17 @@
+
+public class Pattern1 {
+    public static void main(String[] args) {
+        int n=5; //rows
+
+        for (int i=0; i<n; i++){
+            for (int j=0; j<i; j++){ // used for spaces
+                System.out.print(" "); //mistake occured here i use println instead of print
+            }
+            for (int j=0; j<n-i; j++){
+
+                System.out.print("*"); //same mistake occur here
+            }
+            System.out.print("\n");
+        }
+    }
+}
